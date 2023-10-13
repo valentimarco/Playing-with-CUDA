d6bacdff2255aefd4303b606e3403bd9601d0edc
valentimarco <gamespy67@gmail.com>
2023-10-14 01:53:07 +0200

    Delete Introduction/build directory

--- Introduction/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/Cuda/Introduction
-# Build directory: /home/marco/Cuda/Introduction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
