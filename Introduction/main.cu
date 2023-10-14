f726bb4eff27f9d9b0560cbd94bbebfc3f0e3b7b
valentimarco <gamespy67@gmail.com>
2023-10-14 03:34:11 +0200

    Modified main for the Introduction folder

--- Introduction/main.cu
+++ Introduction/main.cu
@@ -8,13 +8,36 @@ __global__ void add(int a, int b, int *c){
 
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    int deviceCount;
+    cudaGetDeviceCount(&deviceCount);
+
+    std::cout << "Hello World! You have " << deviceCount << " available device" << std::endl;
+
+    std::cout << "Info for each device: " << std::endl;
+
+    cudaDeviceProp deviceProp;
+
+    for (int i = 0; i < deviceCount; i++) {
+        cudaGetDeviceProperties(&deviceProp, i);
+        std::cout << "Device " << i << ": " << deviceProp.name << std::endl;
+        std::cout << "TotalGlobalMem" << ": " << deviceProp.totalGlobalMem << "Bytes"<< std::endl;
+        std::cout << "SharedMemPerBlock"<< ": " << deviceProp.sharedMemPerBlock << "Bytes" << std::endl;
+    }
+    memset(&deviceProp, 0, sizeof(cudaDeviceProp));
+    deviceProp.major = 2;
+    deviceProp.minor = 0;
+    
+    std::cout << "Selecting all gpu with compute capability >= 2.0" << std::endl;
+    cudaChooseDevice(&deviceCount, &deviceProp);
+    cudaSetDevice(deviceCount);
+    
+    
 
     int c;
     int *dev_c;
     cudaMalloc((void**)&dev_c, sizeof(int));
     add<<<1,1>>>(2,7,dev_c);
-
+    // host memory can only be access by host code, and vice versa!
     cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
 
     printf("2 + 7 = %d\n", c);
